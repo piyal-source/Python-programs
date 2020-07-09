bae7f917f65baa241303a9f6fe4478489223f824
piyal-source <chakravertypiyal@gmail.com>
2020-07-10 02:10:57 +0530

    Update First n prime numbers.py

--- First n prime numbers.py
+++ First n prime numbers.py
@@ -1,17 +1,17 @@
 n = 12
-a = [_ for _ in range(2,n+1)]
-v = [0]*(n-1)
+num_arr = [_ for _ in range(2,n+1)]
+visited = [0]*(n-1)
 
-p = 0
+prime_index = 0
 while True:
-    for i in range(a[p]*a[p],n+1):
-        if i % a[p] == 0:
-            v[a.index(i)] = 1
-    if v[p+1:n+1].count(0)==0:
+    for i in range(num_arr[prime_index]*num_arr[prime_index],n+1):
+        if i % num_arr[prime_index] == 0:
+            visited[num_arr.index(i)] = 1
+    if visited[prime_index+1:n+1].count(0)==0:
         break
     else:
-        p = v.index(0,p+1,n-1)
-# print(f'{a}\n{v}\n')
+        prime_index = visited.index(0,prime_index+1,n-1)
+# print(f'{num_arr}\n{visited}\n')
 for i in range(n-1):
-    if v[i]==0:
-        print(a[i], end=" ")
+    if visited[i]==0:
+        print(num_arr[i], end=" ")
